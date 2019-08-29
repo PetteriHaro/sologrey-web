e214e51dd469016835637cbda285528ae110ce44
PetteriHaro <petteri.haro@outlook.com>
2019-08-29 22:22:32 +0300

    Mobile friendliness

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import styles from './App.module.css';
-import Background from './assets/images/sologrey-taustakuva.png';
-import Logo from './assets/images/sologrey-logo.png'
-import Tree from './components/tree';
+import Home from './components/Home';
+import Emeco from './components/Emeco';
+import BlaStation from './components/BlaStation';
+import Lapalma from './components/Lapalma';
+import Sancal from './components/Sancal';
+import Rex from './components/Rex';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className={styles.App}>
-      <div className={styles.ImageContainer}>
-        <img src={Background} className={styles.Background} alt="Sologrey Under Construction" />
-      </div>
-      <Tree />
-      <div className={styles.UnderConstruction}>
-        <h1>UNDER CONSTRUCTION</h1>
-      </div>
-      <div className={styles.TopLeftContainer}>
-        <img src={Logo} className={styles.Logo} alt="Sologrey Logo" />
-        <h3>It’s time to take a breath and create a new start for the fall.</h3>
-      </div>
-      <div className={styles.BottomRightContainer}>
-        <h3>CONTACT</h3>
-        <p>Miia Kylmänen</p>
-        <p>miia@sologrey.fi</p>
-        <p>+358 (0) 500 611 722</p>
-      </div>
+      <Home />
+      <Emeco />
+      <BlaStation />
+      <Lapalma />
+      <Sancal />
+      <Rex />
+      <Footer />
     </div>
   );
 }
